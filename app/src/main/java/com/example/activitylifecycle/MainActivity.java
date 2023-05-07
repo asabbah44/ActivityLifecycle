7a88108d80c52fb37a1f88edebeca53690a1abc8
asabbah44 <43098459+asabbah44@users.noreply.github.com>
2023-05-07 23:37:18 +0300

    App to show Activity life cycle

---
+++ app/src/main/java/com/example/activitylifecycle/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.activitylifecycle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+  String tag="Life cycle step : ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.d(tag,"onCreate");
+    }
+
+    public void onClickButton(View v) {
+
+        Intent i = new Intent(this, Second.class);
+        startActivity(i);
+    }
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(getApplicationContext(), "onStart called", Toast.LENGTH_LONG).show();
+
+        Log.d(tag,"onStart");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(getApplicationContext(), "onRestart called", Toast.LENGTH_LONG).show();
+
+         Log.d(tag,"onRestart");
+    }
+
+    @Override
+    protected void onResume() {
+
+        super.onResume();
+        Toast.makeText(getApplicationContext(), "onResumed called", Toast.LENGTH_LONG).show();
+
+         Log.d(tag,"onResume");
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(getApplicationContext(), "onPause called", Toast.LENGTH_LONG).show();
+
+        Log.d(tag,"onPause");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(getApplicationContext(), "onStop called", Toast.LENGTH_LONG).show();
+        Log.d(tag,"onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(tag,"onDestroy");
+    }
+}
